1299535207b3fb5a18e4e8cb8f612892cafceae8
aliravn <alira.vn@gmail.com>
2019-09-13 16:12:35 +0200

    fetch data, display data in a table

--- home.php
+++ home.php
@@ -18,52 +18,63 @@
 	<title>Intergalactic Library</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
-	
 </head>
 
 <body>
- 
-	   <!-- <a href="create.php"><button type="button">Book car</button></a> -->
-	   
-	   <table  border="1" cellspacing= "0" cellpadding="0">
-		   <thead>
-			   <tr>
-				   <th>CarRegNo</th>
-				   <th>DailyRate</th>
-				   <th>Available</th>
-				   <th>CarModel</th>
-				   <th>Location</th>
-				   <th>Reservation</th>
-			   </tr>
-		   </thead>
-		   <tbody>
-			   <?php
-			   // require_once 'dbconnect.php';
-			   $sql = "SELECT cars.registration_num, cars.daily_rate, cars.availability, models.model_name,locations.location_name FROM cars JOIN locations ON locations.location_id = cars.fk_location_id JOIN models ON cars.fk_model_id = models.model_id WHERE cars.availability = 'yes'";
-			   $result = $conn->query($sql); 
 
-				if($result->num_rows > 0) {
-					while($row = $result->fetch_assoc()) { //?? --> durch alle Einträge gehen (joh) //checking how many rows the table has and saving the value in $row. If it is null/0 it will go to else.
-					   echo  "<tr>
-						   <td>" .$row['registration_num']."</td>
-						   <td>" .$row['daily_rate']."</td>
-						   <td>" .$row['availability']."</td>
-						   <td>" .$row['model_name']."</td>
-						   <td>" .$row['location_name']."</td>
-						   <td>
-							   <a href='update.php?id=" .$row['registration_num']."'><button type='button'>Book</button></a>
-						   </td>
-					   </tr>" ;
-				   }
-			   } else  {
-				   echo  "<tr><td colspan='5'><center>No Data Avaliable</center></td></tr>";
-			   }
-				?>
-		   </tbody>
-	   </table>
+<a href="create.php"><button type="button">Add new media</button></a>
 
-	   
- 
+	<table  border="1" cellspacing= "0" cellpadding="0">
+		<thead>
+			<tr>
+				<th>CarRegNo</th>
+				<th>DailyRate</th>
+				<th>Available</th>
+				<th>CarModel</th>
+				<th>Location</th>
+				<th>Reservation</th>
+				<th>Reservation</th>
+				<th>Reservation</th>
+				<th>Reservation</th>
+				<th>Manipulate</th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			$sql_request = "SELECT media_lib_ID, isbn_code, title, concat(first_name, ' ', last_name) as author, cover_image, short_description, publish_date, name, media_type, media_status FROM media JOIN authors ON authors.author_ID = media.fk_author JOIN publishers ON publishers.publisher_ID = media.fk_publisher";
+			$result = $conn->query($sql_request); 
+
+			if($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					echo  
+					"<tr>
+						<td>" .$row['isbn_code']."</td>
+						<td>" .$row['title']."</td>
+						<td>" .$row['author']."</td>
+						<td>
+							<img class='img-thumbnail img-fluid' src=" .$row['cover_image']." alt='some image'/>
+							</td>
+						<td>" .$row['short_description']."</td>
+						<td>" .$row['publish_date']."</td>
+						<td>" .$row['name']."</td>
+						<td>" .$row['media_type']."</td>
+						<td>" .$row['media_status']."</td>
+					<td>
+					<a href='update.php?id=" .$row['registration_num']."'><button type='button'>Show Details</button></a>
+					<a href='update.php?id=" .$row['registration_num']."'><button type='button'>Delete</button></a>
+
+					</td>
+					</tr>" ;
+				}
+			} else  {
+				echo  "<tr><td colspan='5'><center>No Data Avaliable</center></td></tr>";
+			}
+			?>
+		</tbody>
+	</table>
+
+
+
 </body>
 </html>
 
