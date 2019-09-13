8f8b39161354159aa310afe38b50afd76aac1857
aliravn <alira.vn@gmail.com>
2019-09-13 18:21:06 +0200

    add selectbox to update

--- update.php
+++ update.php
@@ -1,21 +1,29 @@
-<!-- File 5: Update form (file: update.php)
-File update.php (update.php) contains an HTML form to update the user’s data, but before we update user’s data we need to display existing data for the selected user. -->
-
 <?php 
+	require_once 'db_connect.php';
 
-require_once 'db_connect.php';
+	if ($_GET['id']) {
+		$id = $_GET['id'];
+		$sql_request = "SELECT media_lib_ID, isbn_code, title, concat(first_name, ' ', last_name) as author, cover_image, short_description, publish_date, name, media_type, media_status FROM media JOIN authors ON authors.author_ID = media.fk_author JOIN publishers ON publishers.publisher_ID = media.fk_publisher WHERE media_lib_ID = '$id'";
+		// var_dump($sql_request);
+		$result = $connect->query($sql_request); 
+		$data = $result->fetch_assoc(); 
 
-if ($_GET['id']) {
-   
-   $id = $_GET['id'];
 
-   $sql_request = "SELECT media_lib_ID, isbn_code, title, concat(first_name, ' ', last_name) as author, cover_image, short_description, publish_date, name, media_type, media_status FROM media JOIN authors ON authors.author_ID = media.fk_author JOIN publishers ON publishers.publisher_ID = media.fk_publisher WHERE media_lib_ID = '$id'";
-   // var_dump($sql_request);
-   $result = $connect->query($sql_request); 
+		$sql_author = "SELECT * FROM authors";
+		$result = $connect->query($sql_author); 
+		$author_list = [];
+		if($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$author_list[$row['author_ID']] = $row['first_name'].' '.$row['last_name'];
+			}
+	}
+// var_dump($author_list);
 
-   $data = $result->fetch_assoc(); 
 
-   $connect->close(); 
+
+
+
+$connect->close(); 
 
 ?>
 
@@ -53,8 +61,16 @@ if ($_GET['id']) {
 
 				<tr>
 					<th>Author</th>
-					<td><input type ="text" name= "author" value= "<?php echo $data['author'] ?>" /></td>
+					<td>
+						<select>
+						<?php foreach($author_list as $author_ID=>$author_name) {
+							echo "<option value=$author_ID>$author_name</option>";
+						}
+						?>
+						</select>
+					</td>
 				</tr>
+
 				<tr>
 					<th>Cover Image Link</th>
 					<td><input type ="text" name= "cover_image" value= "<?php echo $data['cover_image'] ?>" /></td>
@@ -80,8 +96,9 @@ if ($_GET['id']) {
 					<td><input type ="text" name= "media_status" value= "<?php echo $data['media_status'] ?>" /></td>
 				</tr>				
 				<tr>
-					<td><button  type= "submit">Update</button></td>
-					<td><a  href= "home.php"><button type="button">Back</button ></a></td>
+					<td><a href="a_update.php?id=<?php echo $data['media_lib_ID'] ?>"><button type='button'>Update</button></a></td>
+
+					<td><a  href= "home.php"><button type="button">Back</button></a></td>
 				</tr>
 			</table>
 		</form>
@@ -93,3 +110,4 @@ if ($_GET['id']) {
 <?php
 }
 ?>
+
