eaa34e956226dc2f4fe267d2eebb87db2ef95528
aliravn <alira.vn@gmail.com>
2019-09-14 13:45:24 +0200

    add function to display publisher

--- p_publisher.php
+++ p_publisher.php
@@ -10,6 +10,12 @@ if ($_GET['id']) {
 	$id = $_GET['id'];
 	$sql_request = "SELECT media.title FROM media WHERE fk_publisher = '$id'";
 	$result = $connect->query($sql_request); 
+
+	$sql_publisher = "SELECT publishers.name FROM publishers WHERE publisher_ID = '$id'";
+	$result_publisher = $connect->query($sql_publisher); 
+	$publisher_name = $result_publisher->fetch_assoc();
+	var_dump($publisher_name);
+
 	$connect->close(); 
 ?>
 
@@ -17,22 +23,23 @@ if ($_GET['id']) {
 <html>
 <head>
    <title>Media by Publisher</title>
+   <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 
 <body>
-	<fieldset>
-		<?php 
-		if($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) {
-				echo  
-				"<div>" .$row['title']."</div>" ;
-			}
-		} else {
-			echo  "<tr><td colspan='5'><center>No Data Avaliable</center></td></tr>";
+	<h3>List of Media buplished by <?php echo $publisher_name['name'] ?></h3>
+	<?php 
+	if($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			echo  
+			"<div>" .$row['title']."</div>" ;
 		}
-		?>
-		<a  href= "index.php"><button type="button">Back</button></a>
-	</fieldset>
+	} else {
+		echo  "<tr><td colspan='5'><center>No Data Avaliable</center></td></tr>";
+	}
+	?>
+	<a  href= "index.php"><button type="button">Back</button></a>
+
 
 </body>
 </html>
