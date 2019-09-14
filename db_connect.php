e50d00d602af513ec99bdbfa19f9aeb980eee5ba
aliravn <alira.vn@gmail.com>
2019-09-14 16:32:49 +0200

    add file for db connection

---
+++ db_connect.php
@@ -0,0 +1,16 @@
+<?php
+// this will avoid mysql_connect() deprecation error.
+error_reporting( ~E_DEPRECATED & ~E_NOTICE );
+
+define ('DBHOST', 'localhost');
+define('DBUSER' , 'root');
+define('DBPASS', '');
+define ('DBNAME', 'cr11_valeria_nazarova_biglibrary');
+
+$connect = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
+
+if (!$connect) {
+ die("Connection failed : ". mysqli_error());
+}
+// else {echo "connected";}
+?>
