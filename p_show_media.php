a40a600a1b6aa0d8d77ad707c27c4dfc5d6ed741
aliravn <alira.vn@gmail.com>
2019-09-14 16:08:11 +0200

    finish styling show media page

--- p_show_media.php
+++ p_show_media.php
@@ -18,23 +18,26 @@ if ($_GET['id']) {
 <!DOCTYPE html>
 <html>
 <head>
-   <title>Media Details</title>
+	<title>Media Details</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/style.css">   
 </head>
 
 <body>
-	<fieldset>
-		<legend><h1><?php echo $data['title'];?></h1></legend>
+	<div class="show-media-page">
 		<img class='img-thumbnail img-fluid img-size' width="100" src="<?php echo $data['cover_image'] ?>" alt='some image'/> 
-		<p>Title: <?php echo $data['title'] ?></p>
-		<p>Author: <?php echo $data['author'] ?></p>
-		<p>ISBN: <?php echo $data['isbn_code']?></p>		
-		<p>Description: <?php echo $data['short_description'] ?></p>
-		<p>Availability: <?php echo $data['media_status'] ?></p>
-		<button>Reserve</button>
-		<a href="p_update.php?id=<?php echo $data['media_lib_ID'] ?>"><button class='home-manipulate-button' type='button'>Edit</button></a>
-		<a href="p_delete.php?id=<?php echo $data['media_lib_ID'] ?>"><button class='home-manipulate-button' type='button'>Delete</button></a>
-		<a href="index.php"><button type="button">Back</button></a>		
-	</fieldset>
+		<p><strong>Title: </strong><?php echo $data['title'] ?></p>
+		<p><strong>Author: </strong><?php echo $data['author'] ?></p>
+		<p><strong>ISBN: </strong><?php echo $data['isbn_code']?></p>		
+		<p class="desctiption"><strong>Description: </strong><?php echo $data['short_description'] ?></p>
+		<p><strong>Availability: </strong><?php echo $data['media_status'] ?></p>
+		<div class="show-media-button-container">		
+			<a href="index.php"><button type="button">Back</button></a>
+			<a href="p_update.php?id=<?php echo $data['media_lib_ID'] ?>"><button class='home-manipulate-button' type='button'>Edit</button></a>
+			<a href="p_delete.php?id=<?php echo $data['media_lib_ID'] ?>"><button class='home-manipulate-button' type='button'>Delete</button></a>
+			<button>Reserve</button>
+		</div>	
+	</div>
 
 </body>
 </html>
