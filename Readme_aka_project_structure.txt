0d5d993b895b53ca26d31a3f75f561edc8715494
aliravn <alira.vn@gmail.com>
2019-09-14 16:32:08 +0200

    add project description and exported DB file

--- Readme_aka_project_structure.txt
+++ Readme_aka_project_structure.txt
@@ -1,7 +1,25 @@
 Project Structure:
 
 1) Database development and DB creation in mySQL
+Exported DB in SQL-file: cr11_valeria_nazarova_biglibrary.sql
 
-2) App structure and functonality
+2) App structure:
+Flowchart of the app structure: app_structure.jpg
 
-2) Connection mySQL with pHP and HTML
+3) Connection mySQL with pHP and HTML
+"action"-pages:
+db_connect.php
+a_create.php
+a_update.php
+a_delete.php
+
+"static"-pages
+index.php - landing page
+create.php
+update.php
+delete.php
+publisher.php - list all media by one publisher
+show_media.php - show single media data
+
+CSS-folder - pages styles
+
--- cr11_valeria_nazarova_biglibrary.sql
+++ cr11_valeria_nazarova_biglibrary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 14, 2019 at 01:35 PM
+-- Generation Time: Sep 14, 2019 at 04:24 PM
 -- Server version: 5.7.27-0ubuntu0.18.04.1
 -- PHP Version: 7.2.19-0ubuntu0.18.04.2
 
@@ -41,7 +41,7 @@ INSERT INTO `authors` (`author_ID`, `first_name`, `last_name`) VALUES
 (2, 'Ray', 'Bradbury'),
 (3, 'Arthur', 'Conan Doyle'),
 (4, 'John', 'Bon Jovi'),
-(5, 'Rammstein', 'n/a'),
+(5, 'Till', 'Lindermann'),
 (6, 'TEST AUTHOR', 'NAME');
 
 -- --------------------------------------------------------
@@ -70,10 +70,14 @@ CREATE TABLE `media` (
 INSERT INTO `media` (`media_lib_ID`, `isbn_code`, `title`, `fk_author`, `cover_image`, `short_description`, `publish_date`, `fk_publisher`, `media_type`, `media_status`) VALUES
 (4, 'B002SQFZ68', 'Bon Jovi: Live at Madison Square Garden', 4, 'https://images-na.ssl-images-amazon.com/images/I/51cFP1YuUsL.jpg', 'Blu-Ray edition  New York famed Madison Square Garden on this exciting live DVD from New Jersey finest Rock-n-Roll band', '2010-05-15', 4, 'book', 'available'),
 (6, '978-1451678192', 'The Martian Chronicles', 2, 'https://images-na.ssl-images-amazon.com/images/I/518WxaFw0rL._SX307_BO1,204,203,200_.jpg', 'In The Martian Chronicles fine dust settles on the great empty cities of a vanished, devastated civilization', '2012-04-17', 2, 'book', 'available'),
-(14, '12345gtyhyasd', 'test3', 6, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', '3rqfaavdzn', '2019-01-01', 1, 'book', 'reserved'),
-(20, 'rveye', 'adhtrha', 1, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'atrjatr', '2019-01-01', 4, 'cd', 'available'),
-(39, 'qwtcv', 'aeyvevyae', 1, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'aetyhe', '2019-01-01', 1, 'book', 'available'),
-(40, 'qwtcv', 'aeyvevyae', 1, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'aetyhe', '2019-01-01', 1, 'book', 'available');
+(83, '978-1451673319', 'Fahrenheit 451', 2, 'https://images-na.ssl-images-amazon.com/images/I/41qI9quGIdL._SX324_BO1,204,203,200_.jpg', 'Ray Bradburys internationally acclaimed novel Fahrenheit 451 is a masterwork of twentieth-century literature set in a bleak, dystopian future.', '2012-01-10', 2, 'book', 'reserved'),
+(84, '978-0486474915', 'The Adventures of Sherlock Holmes', 3, 'https://images-na.ssl-images-amazon.com/images/I/51NfLyBZK%2BL._SX311_BO1,204,203,200_.jpg', 'Venture back in time to Victorian London to join the detective Sherlock Holmes and his devoted assistant, Dr. Watson as they investigate a dozen of their best-known cases. ', '1996-10-03', 3, 'book', 'reserved'),
+(85, 'B000007VKZ', 'Rammstein - Sehnsucht ', 5, 'https://images-na.ssl-images-amazon.com/images/I/51i4%2B8RVWLL._SY492_BO1,204,203,200_.jpg', 'Awesome 2nd album from 1998 from the German pyromaniacs! Fts. \"Du Hast\"', '2005-04-18', 4, 'cd', 'available'),
+(86, '978-0544003415', 'The Lord of the Rings', 1, 'https://images-na.ssl-images-amazon.com/images/I/51EstVXM1UL._SX331_BO1,204,203,200_.jpg', 'One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them Screen reader support enabled.      		  One Ring to rule them all, One Ring to find them, One Ring to bring them all and in the darkness bind them  ', '2014-08-14', 1, 'book', 'available'),
+(87, '1000000000', 'TITLE_TEST_1', 6, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'Short description for the TEST MEDIA 1', '2019-09-14', 5, 'dvd', 'reserved'),
+(88, '2000000000', 'TITLE_TEST_2', 6, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'Short description for the TEST MEDIA 2', '2019-09-14', 2, 'cd', 'available'),
+(89, '3000000000', 'TITLE_TEST_3', 6, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'Short description for the TEST MEDIA 3', '2019-09-14', 3, 'cd', 'reserved'),
+(90, '4000000000', 'TITLE_TEST_4', 1, 'https://i.ebayimg.com/images/g/CSIAAOSwpCFcaLQV/s-l300.jpg', 'Short description for the TEST MEDIA 4', '2019-09-14', 5, 'book', 'reserved');
 
 -- --------------------------------------------------------
 
@@ -136,7 +140,7 @@ ALTER TABLE `authors`
 -- AUTO_INCREMENT for table `media`
 --
 ALTER TABLE `media`
-  MODIFY `media_lib_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+  MODIFY `media_lib_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=91;
 --
 -- AUTO_INCREMENT for table `publishers`
 --
